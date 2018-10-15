da1faabe87364d9a3dba7bab0ec62dad95e1cf3e
thomasrosales <37603053+thomasrosales@users.noreply.github.com>
2018-10-15 18:40:52 -0300

    Update matrices.h

--- matrices.h
+++ matrices.h
@@ -4,9 +4,9 @@
 int N, i, j, k;
 
 double dwalltime();
-int* matrix_declaration(int *M, int N);
-void init_matriz(int *A, int *R, int *Ai, int M);
-void mullti_matrix(int *R, int *A, int *Ai, int N);
+int* declaration_matrix(int *M, int N);
+void init_matrix(int *A, int *R, int *Ai, int M);
+void multi_matrix(int *R, int *A, int *Ai, int N);
 void print_matrix(int *M, int N, int o);
 void free_matrix(int *A, int *R, int *Ai);
 
