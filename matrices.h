5879a3e9ffc896060f5f0be835dc47e95b13ba02
thomasrosales <37603053+thomasrosales@users.noreply.github.com>
2018-10-15 18:40:17 -0300

    Update matrices.h

--- matrices.h
+++ matrices.h
@@ -8,6 +8,6 @@ int* matrix_declaration(int *M, int N);
 void init_matriz(int *A, int *R, int *Ai, int M);
 void mullti_matrix(int *R, int *A, int *Ai, int N);
 void print_matrix(int *M, int N, int o);
-void free_matriz(int *A, int *R, int *Ai);
+void free_matrix(int *A, int *R, int *Ai);
 
 #endif // MATRICES_H_
