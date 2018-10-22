28505303243d9f01e3b3bc10e95e026abd15d31e
mint <mint@mint-virtual-machine>
2018-10-22 20:17:56 -0300

    prueba de modularización y toma de medidas con el comando size.

--- main.c
+++ main.c
@@ -4,31 +4,29 @@
 #include "matrices.h"
 
 int *A, *Ai, *R;
-double timetick;
 
 int main(int argc, const char* argv[]){
 
-	if (argc < 2){
-    	printf("Falta un parametro.\n");
+    if (argc < 2){
+        printf("Falta un parametro.\n");
     	printf("1. Dimension de la matriz: 512, 1024 y 2048\n");
-    	return 0;
-  	}
+        return 0;
+    }
 
-  	N = atoi(argv[1]);
+    N = atoi(argv[1]);
 
-  	int i, j, k;
-	double timetick;
+    A = declaration_matrix(A, N);
+    Ai = declaration_matrix(Ai, N);
+    R = declaration_matrix(R, N);
 
-	//print_hello();
-	A = declaration_matrix(A, N);
-	Ai = declaration_matrix(Ai, N);
-	R = declaration_matrix(R, N);
-	
-	init_matrix(A, R, Ai,N);
+    init_matrix(A, R, Ai, N);
 
-	multi_matrix(R, A, Ai,N);
+    multi_matrix(R, A, Ai, N);
 
-	print_matrix(R,N,0);
+    print_matrix(A, N, 0);
+    print_matrix(Ai, N, 0);
+    print_matrix(R, N, 0);
 
-	free_matrix(A,R,Ai);
+    free_matrix(A, R, Ai);
+
 }
