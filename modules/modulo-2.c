d781d49345e40f31098e9b3bff08b31684c6bf35
Tomás Rosales <tomasjoaquinrosales@gmail.com>
2018-10-26 17:21:28 -0300

    modulos.

---
+++ modules/modulo-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include "matrices.h"
+#define N 512
+
+int *A, *Ai, *R;
+
+int main(int argc, const char* argv[]){
+
+    N = atoi(argv[1]);
+
+    A = declaration_matrix(A, N);
+    Ai = declaration_matrix(Ai, N);
+
+}
---
+++ modules/modulo-6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include "matrices.h"
+#define N 512
+
+int *A, *Ai, *R;
+
+int main(int argc, const char* argv[]){
+
+    N = atoi(argv[1]);
+
+    A = declaration_matrix(A, N);
+    Ai = declaration_matrix(Ai, N);
+    R = declaration_matrix(R, N);
+
+    init_matrix(A, R, Ai, N);
+
+    multi_matrix(R, A, Ai, N);
+
+    print_matrix(R, N, 0);
+
+
+}
